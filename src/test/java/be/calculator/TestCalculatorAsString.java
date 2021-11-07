d191e4f9f791c743be4f923d18e2cd428300f7eb
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-11-07 12:41:34 +0100

    extra testen

--- src/test/java/be/calculator/TestCalculatorAsString.java
+++ src/test/java/be/calculator/TestCalculatorAsString.java
@@ -9,121 +9,136 @@ public class TestCalculatorAsString {
 
     @Test
     void emptyCode() {
-        assertEquals("empty code = 0", calculator.getResultAsString(""));
+        assertEquals(0, calculator.getResult(""));
     }
 
     @Test
     void randomCode() {
-        assertEquals("invalid code = 0", calculator.getResultAsString(" hhj s"));
+        assertEquals(0, calculator.getResult(" hhj s"));
     }
 
     @Test
     void nullCode() {
-        assertEquals("null code = 0", calculator.getResultAsString(null));
+        assertEquals(0, calculator.getResult(null));
     }
 
     @Test
     void simpleCode() {
-        assertEquals("5 + 4 + 3 + 7 = 19", calculator.getResultAsString("5,4,3,7"));
+        assertEquals(19, calculator.getResult("5,4,3,7"));
     }
 
     @Test
     void simpleCodeWithSpaces() {
-        assertEquals("5 + 4 + 3 + 7 = 19", calculator.getResultAsString("   5,   4,3, 7 "));
+        assertEquals(24, calculator.getResult("   5,   4,8, 7 "));
     }
 
     @Test
     void simpleCodeWithOther() {
-        assertEquals("invalid code = 0", calculator.getResultAsString(" aav 5,4,3, 7 "));
+        assertEquals(0, calculator.getResult(" aav 15,4,3, 7 "));
     }
 
     @Test
     void simpleCodeParentheses() {
-        assertEquals("5 + 4 + 3 + 7 = 19", calculator.getResultAsString("(5,4,3,7)"));
+        assertEquals(29, calculator.getResult("(15,4,3,7)"));
     }
 
     @Test
     void simpleCodeParenthesesAndSpaces() {
-        assertEquals("5 + 4 + 3 + 7 = 19", calculator.getResultAsString("(  5, 4,3, 7 )"));
+        assertEquals(25, calculator.getResult("(  5, 4,9, 07 )"));
     }
 
     @Test
     void simpleCodeParenthesesAndOther() {
-        assertEquals("invalid code = 0", calculator.getResultAsString("( aaab 5, 4,3, 7 )"));
+        assertEquals(0, calculator.getResult("( aaab 5, 4,30, 7 )"));
     }
 
     @Test
     void emptyCodeParentheses() {
-        assertEquals("empty code = 0", calculator.getResultAsString("()"));
+        assertEquals(0, calculator.getResult("()"));
     }
 
     @Test
     void basisCodePlus() {
-        assertEquals("5 + 4 + 3 + 7 = 19", calculator.getResultAsString("&+(5,4,3,7)"));
+        assertEquals(21, calculator.getResult("&+(5,4,3,9)"));
     }
 
     @Test
     void basisCodePlusAndEmptyStart() {
-        assertEquals("5 + 4 + 3 + 7 = 19", calculator.getResultAsString("   &+(5,4,3,7)"));
+        assertEquals(32, calculator.getResult("   &+(5,4,13,3,7)"));
     }
 
     @Test
     void basisCodePlusAndOtherStart() {
-        assertEquals("invalid code = 0", calculator.getResultAsString("ahhd   &+(5,4,3,7)"));
+        assertEquals(0, calculator.getResult("ahhd   &+(5,4,13,3,7)"));
     }
 
     @Test
     void basisCodePlusAndEnd() {
-        assertEquals("5 + 4 + 3 + 7 = 19", calculator.getResultAsString("&+(5,4,3,7) "));
+        assertEquals(26, calculator.getResult("&+(5,4,3,7,4,3) "));
     }
 
     @Test
     void basisCodePlusAndRandom() {
-        assertEquals("invalid code = 0", calculator.getResultAsString("&+(5,4,3,7)aaa5 "));
+        assertEquals(0, calculator.getResult("&+(5,4,3,7)aaa5 "));
     }
 
     @Test
     void basisCodeMin() {
-        assertEquals("5 - 4 - 3 - 7 = -9", calculator.getResultAsString("&-(5,4,3,7)"));
+        assertEquals(-9, calculator.getResult("&-(5,4,3,7)"));
     }
 
     @Test
     void basisCodeMinAndEnd() {
-        assertEquals("5 - 4 - 3 - 7 = -9", calculator.getResultAsString("&-(5,4,3,7) "));
+        assertEquals(-9, calculator.getResult("&-(5,4,3,7) "));
     }
 
     @Test
     void basisCodeMinAndRandom() {
-        assertEquals("invalid code = 0", calculator.getResultAsString("&-(5,4,3,7)aaa5 "));
+        assertEquals(0, calculator.getResult("&-(5,4,3,7)aaa5 "));
     }
 
     @Test
     void basisCodeProdMin() {
-        assertEquals("5 * 4 * 3 * 7 = 420", calculator.getResultAsString("&*(5,4,3,7)"));
+        assertEquals(420, calculator.getResult("&*(5,4,3,7)"));
     }
 
     @Test
     void basisCodeProdAndEnd() {
-        assertEquals("5 * 4 * 3 * 7 = 420", calculator.getResultAsString("&*(5,4,3,7) "));
+        assertEquals(420, calculator.getResult("&*(5,4,3,7) "));
     }
 
     @Test
     void basisCodeProdAndRandom() {
-        assertEquals("invalid code = 0", calculator.getResultAsString("&*(5,4,3,7)aaa5 "));
+        assertEquals(0, calculator.getResult("&*(5,4,3,7)aaa5 "));
     }
 
     @Test
     void basisCodeDiv() {
-        assertEquals("5 / 4 / 3 / 7 = 0.0595", calculator.getResultAsString("&/(5,4,3,7)"));
+        assertEquals(0.05952381, calculator.getResult("&/(5,4,3,7)"), 0.0001);
     }
 
     @Test
     void basisCodeDivAndEnd() {
-        assertEquals("5 / 4 / 3 / 7 = 0.0595", calculator.getResultAsString("&/(5,4,3,7) "));
+        assertEquals(0.05952381, calculator.getResult("&/(5,4,3,7) "), 0.0001);
     }
 
     @Test
     void basisCodeDivAndRandom() {
-        assertEquals("invalid code = 0", calculator.getResultAsString("&/(5,4,3,7)aaa5 "));
+        assertEquals(0, calculator.getResult("&/(5,4,3,7)aaa5 "));
+    }
+
+    @Test
+    void simpleCodeWithNumbersContaining0() {
+        assertEquals(128, calculator.getResult("100,20,8"));
+    }
+
+    @Test
+    void simpleCodeWith0() {
+        assertEquals(5, calculator.getResult("&/(0,20,4,0)"));
+    }
+
+    @Test
+    void simpleCodeWithAll0() {
+        assertEquals(0, calculator.getResult("&/(0,0,0,0)"));
     }
 }
