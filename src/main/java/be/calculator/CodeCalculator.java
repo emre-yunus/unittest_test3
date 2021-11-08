6fec5c5ab55ba631994cd5a46ec128ef65a67698
emre-yunus <r0664998@student.thomasmore.be>
2021-11-08 12:44:44 +0100

    c

--- src/main/java/be/calculator/CodeCalculator.java
+++ src/main/java/be/calculator/CodeCalculator.java
@@ -7,15 +7,32 @@ import java.util.regex.Pattern;
 public class CodeCalculator {
     private CodeParts parts;
 
+    public String getResultAsString(String code) {
+        if (code==null) return "null code = 0";
+        if (code.equals("") || code.equals("()")) return "empty code = 0";
+        if (isCodeOk(code)) {
+            return calculateAsString();
+        }
+        return "invalid code = 0";
+    }
+
+    private String calculateAsString() {
+        String result = "";
+        for (int i = 0; i<parts.numbers.length; i++) {
+            result += parts.numbers[i];
+            if(i!= parts.numbers.length - 1) result += parts.operation;
+            if (i==parts.numbers.length-1) {
+                result += "=";
+            }
+        }
+        return result;
+    }
+
     public double getResult(String code) {
         if (isCodeOk(code)) {
             return calculate();
         }
         return 0;
-    }
-
-    public String getResultAsString(String code) {
-        return "";
     }
 
     private boolean isCodeOk(String code) {
@@ -53,6 +70,7 @@ public class CodeCalculator {
     private static class CodeParts {
         private final String operation;
         private final Integer[] numbers;
+        private boolean zerosOnly = true;
 
         public CodeParts(String operation, String numbers) {
             if (operation.length()<2) {
@@ -60,6 +78,7 @@ public class CodeCalculator {
             } else {
                 this.operation = operation.substring(1);
             }
+
             this.numbers = Arrays.stream(numbers.split(",")).map(s -> Integer.parseInt(s.trim())).filter(i -> i!=0).toArray(Integer[]::new);
         }
     }
